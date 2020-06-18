52edceaa4686fb8b1c4e4e60c817abddca643185
Adam K <gnqs@o2.pl>
2020-06-18 17:14:32 +0100

    subtle colour changes

--- src/App/Theme.jsx
+++ src/App/Theme.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { defaultTheme, darkTheme } from './defaultTheme';
+import { defaultTheme, darkTheme } from './Themes';
 import { ThemeProvider } from 'styled-components';
 
 const Theme = ({ children, theme }) => (
